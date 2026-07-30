807b6c2727d7e4b02b5a4d3a7c6e3c1a4a35b532
alastorrrrr103-jpg <alastorrrrr103@gmail.com>
2026-07-30 10:34:50 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "darknesschat-9ef50.firebaseapp.com",
+  projectId: "darknesschat-9ef50",
+  storageBucket: "darknesschat-9ef50.firebasestorage.app",
+  messagingSenderId: "731739451193",
+  appId: "1:731739451193:web:a3c293018348126411c2e8",
+  measurementId: "G-FBRWEQTN7C"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
